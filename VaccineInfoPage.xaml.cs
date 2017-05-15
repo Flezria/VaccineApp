84d3a001926e7662e21e87663bf8a60718174a7a
Filip Andersen <flezria@gmail.com>
2017-05-15 19:05:29 +0200

    Add files via upload
    Added image trigger til popup window - kan nu lukkes via kryds i hjørne.

---
+++ VaccineInfoPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VaccineApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VaccineInfoPage : PopupPage
+    {
+        public VaccineInfoPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new ViewModel.VaccineInfoViewModel();
+
+        }
+    }
+}
